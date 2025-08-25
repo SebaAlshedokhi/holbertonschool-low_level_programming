c4ddd3b3f7150c718ef5efbfffc4b95b802a96a9
root <root@ip-10-42-193-112.ec2.internal>
2025-08-25 16:59:36 +0000

    Update and Fix

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,15 @@
-#!/bin/bash
-/**
- * main - printf
- * Return: will return 0
- */
-gcc -Wall clean.c -o clean
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints "with proper grammar, but the outcome is a piece of art,"
+ *              using printf, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
