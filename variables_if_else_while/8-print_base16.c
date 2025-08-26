fd1e7e881d030406a6031a20ace61165b0e8410d
root <root@ip-10-42-186-215.ec2.internal>
2025-08-26 22:23:35 +0000

    Update and Fix

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 /**
 * main -  a program that prints all the numbers of base 16 in lowercase.
-* Descriptioni: Write  a program that prints all the numbers of base 16 in lowercase.
+* Descriptioni: A program that prints all the numbers of base 16 in lowercase.
 * Return: Always 0 (Success)
 */
 
