2cbf88be33d7f755fede5c73302233658c683768
root <root@ip-10-42-101-88.ec2.internal>
2025-08-26 11:31:14 +0000

    Update and Fix

---
+++ hello_world/6-size.c
@@ -0,0 +1,21 @@
+/**
+ * function_name - Short description, single line
+ * @parameterx: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: Description of the returned value
+ */
+#include<stdio.h>
+int main(void)
+{
+	printf(sizeof(char));
+	printf(sizeof(short));
+	printf(sizeof(int));
+	printf(sizeof(double));
+	printf(sizeof(long));
+	printf(sizeof(long long));
+	printf(sizeof(float));
+	printf(sizeof(long double));
+	return (0);
+}
