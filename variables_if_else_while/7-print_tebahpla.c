94ebc4e508fe5c984817bdedf0d5cb970dba007b
root <root@ip-10-42-186-215.ec2.internal>
2025-08-26 21:36:04 +0000

    Update and Fix

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+/**
+* main -  a program that prints the alphabet in lowercase.
+* Descriptioni: This program use putchar to print the alphabet in lower case
+* Return: Always 0 (Success)
+*/
+
+#include<stdio.h>
+
+int main(void)
+{
+char c = 'z';
+
+while (c >= 'a')
+{
+putchar(c);
+c--;
+}
+putchar('\n');
+
+return (0);
+}
