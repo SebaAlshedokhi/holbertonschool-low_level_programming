3c7d92036517d12b0f789aa97207bd5f3b0d71c5
root <root@ip-10-42-186-215.ec2.internal>
2025-08-26 22:37:32 +0000

    Update and Fix

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+/**
+* main -  a program that prints all the numbers of base 16 in lowercase.
+* Descriptioni: A program that prints all the numbers of base 16 in lowercase.
+* Return: Always 0 (Success)
+*/
+
+#include<stdio.h>
+
+int main(void)
+{
+int n = 48;
+
+while (n <57 )
+{
+putchar(n);
+n++;
+putchar(44);
+putchar(32);
+}
+putchar(57);
+
+return (0);
+}
