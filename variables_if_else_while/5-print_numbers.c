7310c9ce48bb5e0c672f404870a5ecb8be9357a4
root <root@ip-10-42-186-215.ec2.internal>
2025-08-26 21:12:39 +0000

    Update and Fix

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+/**
+* main -  a program that prints all single digit numbers.
+* Descriptioni: Write a program that prints all single digit numbers of base 10.
+* Return: Always 0 (Success)
+*/
+
+#include<stdio.h>
+
+int main(void)
+{
+int s;
+
+for (s = 0; s < 10; s++)
+printf ("%d", s);
+printf("\n");
+
+return (0);
+}
