de20b866bb6ebf0787c35fb63776a07ced443227
root <root@ip-10-42-205-124.ec2.internal>
2025-08-26 16:59:20 +0000

    Update and Fix

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,6 +1,6 @@
 /**
 * main - find and compair with a number
-* Description : This program will print the last digit of the number stored in the variable n.
+* Description : This program will print last digit of the variable n.
 * Return: Always 0 (Success)
 */
 
@@ -14,20 +14,20 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if ((n % 10) > 5) 
+	if ((n % 10) > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n",n,(n % 10));
+		printf("Last digit of %d is %d and is greater than 5\n", n, (n % 10));
 	}
 	else
 	{ 
 		if ((n % 10) == 0)
 		{
-			printf("Last digit of %d is %d and is 0\n",n,(n % 10));
+			printf("Last digit of %d is %d and is 0\n", n, (n % 10));
 		}
 		else
 			if ((n % 10) < 6)
 			{
-			printf("Last digit of %d is %d and is less than 6 and not 0\n",n,(n % 10));
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, (n % 10));
 			}
 	}
 
