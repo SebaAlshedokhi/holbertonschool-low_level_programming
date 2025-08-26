37c41d340ec1f9ae20a5ca098053785188c0e87a
root <root@ip-10-42-101-88.ec2.internal>
2025-08-26 11:51:15 +0000

    Update and Fix

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,12 +10,9 @@
 int main(void)
 {
 	printf("Size of a char: %u byte(s)\n",sizeof(char));
-	printf("Size of a short: %u byte(s)\n",sizeof(short));
 	printf("Size of a int: %u byte(s)\n",sizeof(int));
-	printf("Size of a double: %u byte(s)\n",sizeof(double));
 	printf("Size of a long: %u byte(s)\n",sizeof(long));
 	printf("Size of a long long: %u byte(s)\n",sizeof(long long));
 	printf("Size of a float: %u byte(s)\n",sizeof(float));
-	printf("Size of a long double: %u byte(s)\n",sizeof(long double));
 	return (0);
 }
