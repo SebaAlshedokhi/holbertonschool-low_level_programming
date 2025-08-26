3776d12603f9ee50eca83eae1f26ce6c2d02ae74
root <root@ip-10-42-186-215.ec2.internal>
2025-08-26 20:45:51 +0000

    Update and Fix

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 /**
 * main -  a program that prints the alphabet in lowercase.
-* Description: This program use putchar function to print the alphabet in lower case
+* Descriptioni: This program use putchar to print the alphabet in lower case
 * Return: Always 0 (Success)
 */
 
