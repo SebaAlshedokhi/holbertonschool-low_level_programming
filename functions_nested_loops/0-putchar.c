f071fbc8fe7c75ab8255671353f780f8ee826d23
root <root@ip-10-42-183-58.ec2.internal>
2025-08-27 14:32:24 +0000

    Update and Fix

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+/**
+* main -  program prints _putchar.
+*
+* Description: This program prints _putchar follwed by new line.
+* Return: Always 0 (Success)
+*/
+
+#include <unistd.h>
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This the function nested loop directory
