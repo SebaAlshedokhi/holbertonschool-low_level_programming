901d0023e4cc6af9e1f95013587858255bddb2d7
root <root@ip-10-42-154-10.ec2.internal>
2025-08-28 22:17:21 +0000

    Update and Fix

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+/**
+* _abs - computes the absolute value .
+* @c : int to checked
+* Description:  a function that computes the absolute value of an integer.
+* Return: absolute value.
+*/
+
+#include"main.h"
+
+int _abs(int c)
+{
+	if (c >= 0)
+		return (c);
+	else
+		return (-c);
+}
