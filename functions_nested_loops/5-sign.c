c0498b6ba0b01a48df6704146f786a26174ef4a9
root <root@ip-10-42-154-10.ec2.internal>
2025-08-28 22:08:39 +0000

    Update and Fix

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+/**
+* print_sign - prints the sign of a number.
+* @n : int to checked
+* Description:  a function that prints the sign of a number.
+* Returns 1 and prints + if n is greater than zero, 0 and prints 0 if n is zero, -1 and prints - if n is less than zero
+*/
+
+#include"main.h"
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		-putchar('+');
+		return (1);
+	}
+	else
+	{
+		if (n == 0)
+		{
+			-putchar('0');
+			return (0);
+		}
+		else
+		{
+			-putchar('-');
+			return (-1);
+		}
+	}
+}
