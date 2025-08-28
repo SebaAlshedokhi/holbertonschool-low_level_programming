abd8e4974b584eb7a82b656838cb1f3901824694
root <root@ip-10-42-154-10.ec2.internal>
2025-08-28 22:05:12 +0000

    Update and Fix

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+/**
+* _isalpha - a function that checks for alphabetic characte
+* @c : int to checked
+* Description:  a function that checks for alpha character,
+* print 1 if c is letter and 0 otherwise.
+* Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+*/
+
+#include"main.h"
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122)||(c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
