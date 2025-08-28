6e63b655b17d19fe22f6610480c222f683a6d3ea
root <root@ip-10-42-154-10.ec2.internal>
2025-08-28 21:57:18 +0000

    Update and Fix

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,7 @@
 /**
 * print_alphabet_x10 - function that prints 10 times
-* Description: a function that prints 10 times the alphabet, in lowercase, followed by a new line.
+* Description: a function that prints 10 times the alphabet,
+* in lowercase, followed by a new line.
 */
 
 #include"main.h"
