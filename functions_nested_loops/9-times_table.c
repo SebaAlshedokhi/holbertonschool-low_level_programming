af6786895a5e961d18878ee05e3a173e5d442bc2
root <root@ip-10-42-164-98.ec2.internal>
2025-08-29 22:05:09 +0000

    Update and Fix

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -13,11 +13,23 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			mult = i * j;
+		mult = i * j;
+		
+		if (mult >= 9)
+		{
+			_putchar((mult / 10) + '0');
+			_putchar((mult % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		else
+		{
 			_putchar(mult + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
+		}
 		_putchar('\n');
 	}
 }
+
