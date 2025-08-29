91c9401d8ec991d2753595c3fbc69da4e4992066
root <root@ip-10-42-154-10.ec2.internal>
2025-08-29 00:24:14 +0000

    Update and Fix

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,16 +10,9 @@ int print_last_digit(int c)
 {
 	int a;
 
-	if (c >= 0)
-	{
-		a = c % 10;
-		_putchar(a + '0');
-		return (a);
-	}
-	else
-	{
-		a = -c % 10;
-		_putchar(a + '0');
-		return (a);
-	}
+	a = c % 10;
+	if (c < 0)
+		a = -a;
+	_putchar(a + '0');
+	return (a);
 }
