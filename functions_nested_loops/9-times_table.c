dbb85071fad63b74018b54baf9a98dc8fd04292d
root <root@ip-10-42-164-98.ec2.internal>
2025-08-29 23:10:28 +0000

    Update and Fix

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,40 +4,42 @@
 *
 */
 
-#include"main.h"
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
 void times_table(void)
 {
-	int i, j, mult;
+	int i, j, prod;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			mult = i * j;
+			prod = i * j;
 
-			if ((i >= 2) && (j <= 5))
+			if (j == 0)
 			{
-				for (i = 2; i <= 9; i++)
-				{
-					for (j = 5; j <= 2; j--)
-					{
-						_putchar((mult / 10) + '0');
-						_putchar((mult % 10) + '0');
-						_putchar(',');
-						_putchar(' ');
-					}
-				}
+				_putchar(prod + '0');
 			}
 			else
 			{
-						_putchar((mult / 10) + '0');
-						_putchar((mult % 10) + '0');
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
+				_putchar(',');
+				_putchar(' ');
+
+				if (prod < 10)
+				{
+					_putchar(' ');
+					_putchar(prod + '0');
+				}
+				else
+				{
+					_putchar((prod / 10) + '0');
+					_putchar((prod % 10) + '0');
+				}
 			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
-
