d07f53bbdeb011369eacc0299927e124589373a1
root <root@ip-10-42-36-102.ec2.internal>
2025-08-29 09:29:40 +0000

    Update and Fix

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,5 +1,5 @@
 /**
-* void jack_bauer -  function that prints every minute of the day.
+* jack_bauer -  function that prints every minute of the day.
 * Description :  a function that prints every minute
 * of the day of Jack Bauer, starting from 00:00 to 23:59.
 */
