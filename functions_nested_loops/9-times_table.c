21083991090ee78289444d43fc224f0b018755f3
root <root@ip-10-42-164-98.ec2.internal>
2025-08-29 21:46:56 +0000

    Update and Fix

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+/**
+* times_table -  prints the 9 times table
+* Description: a function that prints the 9 times table, starting with 0.
+*
+*/
+
+#include"main.h"
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			mult = i * i;
+			_putchar(mult + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
