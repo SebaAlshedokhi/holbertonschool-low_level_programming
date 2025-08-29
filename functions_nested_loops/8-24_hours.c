df0a8f8586de1a0383bf0963810d8951de0265ac
root <root@ip-10-42-154-10.ec2.internal>
2025-08-29 00:37:10 +0000

    Update and Fix

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -12,16 +12,21 @@ void jack_bauer(void)
 	{
 		for (h = 0; h <= 3; h++)
 		{
-			for (M = 0; M <= 5; M++)
+			if (H == 2 && h == 3)
+				exit (0);
+			else
 			{
-				for (m = 0; m <= 9; m++)
+				for (M = 0; M <= 5; M++)
 				{
-					_putchar(H + '0');
-					_putchar(h + '0');
-					_putchar(':');
-					_putchar(M + '0');
-					_putchar(m + '0');
-					_putchar('\n');
+					for (m = 0; m <= 9; m++)
+					{
+						_putchar(H + '0');
+						_putchar(h + '0');
+						_putchar(':');
+						_putchar(M + '0');
+						_putchar(m + '0');
+						_putchar('\n');
+					}
 				}
 			}
 		}
