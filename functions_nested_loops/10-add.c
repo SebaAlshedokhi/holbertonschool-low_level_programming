54edaf62b5541a6d356cdb1aca35beb1ba7c634e
root <root@ip-10-42-164-98.ec2.internal>
2025-08-30 00:06:12 +0000

    Update and Fix

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -12,14 +12,5 @@ int add(int a, int b)
 	int result;
 
 	result = a + b;
-
-	if (result > 9)
-	{
-		_putchar((result / 10) + '0');
-		_putchar((result % 10) + '0');
-	}
-	else
-		_putchar(result + '0');
-	_putchar('\n');
 	return(result);
 }
