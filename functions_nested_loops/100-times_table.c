ebf34951435787ed319988cac54d2b0a665ed90b
root <root@ip-10-42-139-249.ec2.internal>
2025-08-30 12:45:39 +0000

    Update and Fix

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -3,52 +3,46 @@
 * @n : times table.
 * Description: a function that prints the n times table, starting with 0.
 */
-
-#include"main.h"
+#include "main.h"
 void print_times_table(int n)
 {
-	if ((n <= 15) && (n >= 0))
+int i, j, prod;
+if (n >= 0 &&  n <=  15)
+{
+	for (i = 0; i <= n; i++)
 	{
-		int i, j, prod;
-
-		for (i = 0; i <= n; i++)
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j <= n; j++)
+			prod = i * j;
+			if (j != 0)
 			{
-				prod = i * j;
-				if (j == 0)
-				{
-					_putchar(prod + '0');
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					if (prod < 10)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(prod + '0');
-					}
-					else
-					{
-						if (prod >= 100)
-						{
-							_putchar((prod / 100) + '0');
-							_putchar(((prod % 100) / 10) + '0');
-							_putchar(((prod % 100) % 10) + '0');
-						}
-						else
-						{
-							_putchar(' ');
-							_putchar((prod / 10) + '0');
-							_putchar((prod % 10) + '0');
-						}
-					}
-
-				}
+				_putchar(',');
+				_putchar(' ');
 			}
-			_putchar('\n');
+			if (prod < 10)
+			{
+				if (j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+			_putchar(prod + '0');
+			}
+			else if (prod < 100)
+			{
+				if (j != 0)
+					_putchar(' ');
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
+			else
+			{
+				_putchar((prod / 100) + '0');
+				_putchar(((prod / 10) % 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
 		}
+		_putchar('\n');
 	}
+}
 }
