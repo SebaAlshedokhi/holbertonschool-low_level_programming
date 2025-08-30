d527c4ef2d3ad4bc8c896ff7696093ee6d6ee5e9
root <root@ip-10-42-139-249.ec2.internal>
2025-08-30 13:42:27 +0000

    Update and Fix

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -8,16 +8,18 @@
 #include<stdio.h>
 int main(void)
 {
-int i, third;
-int first = 1;
-int second = 2;
+	int i, third;
+	int first = 1;
+	int second = 2;
 
-printf("%d, %d", first, second);
-for (i = 3; i <= 50; i++)
-{
-	third = first + second;
-	printf(", %d", third);
-	first = second;
-	second = third;
+	printf("%d, %d", first, second);
+	for (i = 3; i <= 50; i++)
+	{
+		third = first + second;
+		printf(", %d", third);
+		first = second;
+		second = third;
+	}
+	printf("\n");
+	return (0);
 }
-printf("\n");
