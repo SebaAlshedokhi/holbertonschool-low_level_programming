9c17c44e25b33d094a418d8921abbcf7fe5689bf
root <root@ip-10-42-139-249.ec2.internal>
2025-08-30 13:13:08 +0000

    Update and Fix

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+/**
+* main - sum of product.
+*
+* Description: To print the sum of 3 and 5 multiples.
+* Return: Always 0.
+*/
+
+#include<stdio.h>
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0)
+			sum+ = i;
+		else
+		{
+			if ((i % 5) == 0)
+			sum+ = i;
+		}
+	}
+	print("The sum of the product of 3 and 5 is: %d", sum);
+	return (0);
+}
