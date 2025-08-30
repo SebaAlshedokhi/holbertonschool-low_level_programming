5921c3f9b11100047afecd6b2b1232f266fe48d2
root <root@ip-10-42-139-249.ec2.internal>
2025-08-30 14:43:30 +0000

    Update and Fix

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -1,12 +1,12 @@
 /**
- * main - sum of the even-valued.
+ * fibo - sum of the even-valued.
  *
  * Description:  finds and prints the sum of the even-valued terms of fibonacci.
  * Return: Always 0
  */
 
 #include<stdio.h>
-int main(void)
+int fibo(void)
 {
 	int first = 1;
 	int second = 2;
