86e1880d26c76851e95d0444fdb985cad8260daf
root <root@ip-10-42-74-22.ec2.internal>
2025-08-31 16:10:06 +0000

    Update and Fix

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+/**
+* print_most_numbers - prints numbers.
+*
+* Description:  a function that prints the numbers, from 0 to 9.
+*
+*/
+
+#include"main.h"
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if ((i !== 2) && (i !== 4))
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
