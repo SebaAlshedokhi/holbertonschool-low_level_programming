aada95d88c02b45b2b7522125d54ba6f8deab266
root <root@ip-10-42-74-22.ec2.internal>
2025-08-31 15:56:13 +0000

    Update and Fix

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+/**
+* print_numbers - prints numbers.
+*
+* Description:  a function that prints the numbers, from 0 to 9.
+*
+*/
+
+#include"main.h"
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar('\n');
+}
