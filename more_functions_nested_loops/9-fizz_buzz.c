d4a6a558e12824ac3e1205ec46c0a2826d4a52c9
root <root@ip-10-42-217-126.ec2.internal>
2025-08-31 21:45:37 +0000

    Update and Fix

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,21 +13,15 @@ void main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3) == 0)
-		{
 			printf("Fizz ");
-		}
 		else
-		{
 			if ((i % 5) == 0)
 				printf("Buzz ");
-		}
-		else
-		{
-			if (((i % 3) == 0) && ((i % 5) == 0))
-			printf("FizzBuzz ");
-		}
-		else
-			printf("%d ",i);
+			else
+				if (((i % 3) == 0) && ((i % 5) == 0))
+					printf("FizzBuzz ");
+				else
+					printf("%d ",i);
 	}
 	printf("\n");
 }
