bd8afcb14af09b72715c09c7871896249b45ed30
root <root@ip-10-42-74-22.ec2.internal>
2025-08-31 16:27:11 +0000

    Update and Fix

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+/**
+* 6-print_line - draws a straight line.
+* @n : long of the line.
+* Description:  a function that draws a straight line in the terminal.
+*
+*/
+
+#include"main.h"
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
