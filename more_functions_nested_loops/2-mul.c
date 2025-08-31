7a09c9c1c277725639e6b5bc80c2f1307cb7dfe6
root <root@ip-10-42-74-22.ec2.internal>
2025-08-31 15:53:31 +0000

    Update and Fix

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+/**
+* mul - multiplies two integers.
+* @a: to multiply.
+* @b: to multiply.
+* Description: a function that multiplies two integers.
+* Return: multiple.
+*/
+
+#include"main.h"
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
