be25774b7f25b283e417f2c000cb47a7b419497a
root <root@ip-10-42-217-126.ec2.internal>
2025-08-31 22:25:41 +0000

    Update and Fix

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -10,7 +10,7 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	if (size >= 0)
+	if (size > 0)
 	{
 		for (i = 1; i <= size; i++)
 		{
@@ -27,5 +27,9 @@ void print_triangle(int size)
 			}
 			_putchar('\n');
 		}
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
