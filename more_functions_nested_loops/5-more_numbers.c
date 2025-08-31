44522446662be86d3f2195534a63ffa0e31bc97c
root <root@ip-10-42-74-22.ec2.internal>
2025-08-31 16:13:45 +0000

    Update and Fix

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+/**
+* more_numbers - prints numbers.
+*
+* Description:  a function that prints the numbers, from 0 to 14.
+*
+*/
+
+#include"main.h"
+void more_numbers(void);
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+				_putchar(49);
+				_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
