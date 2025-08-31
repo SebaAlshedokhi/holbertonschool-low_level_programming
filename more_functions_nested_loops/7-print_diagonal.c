5ca10636db20ff9ceb5bcfe5c3d2fff073005d1d
root <root@ip-10-42-74-22.ec2.internal>
2025-08-31 16:54:34 +0000

    Update and Fix

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+/**
+* print_diagonal - draws a straight line.
+* @n : long of the line.
+* Description:  a function that draws a straight line in the terminal.
+*
+*/
+
+#include"main.h"
+void print_diagonal(int n)
+{
+int i, j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (i = 1; i <= n; i++)
+			{
+				if (i == j)
+				{
+					_putchar('\\');
+				}
+			}
+		}
+	}
+	_putchar('\n');
+}
