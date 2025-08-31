18085fdbb933518869aad08aa369708088e43a8e
root <root@ip-10-42-217-126.ec2.internal>
2025-08-31 22:23:35 +0000

    Update and Fix

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+/**
+* print_triangle - prints a triangle.
+* @size: size of the triangle.
+* Description: a function that prints a triangle.
+*
+*/
+
+#include"main.h"
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size >= 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size; j <= 1; j--)
+			{
+				if (i < j)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
