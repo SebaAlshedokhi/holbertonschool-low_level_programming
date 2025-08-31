88b439ae746923e138403464a184764582a0c244
root <root@ip-10-42-74-22.ec2.internal>
2025-08-31 15:51:12 +0000

    Update and Fix

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+/**
+* _isupper - checks for uppercase character.
+* @c: to compare.
+* Description: a function that checks for uppercase character.
+* Return: 1 if c is uppercase and 0 otherwise.
+*/
+
+#include"main.h"
+
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+		return (1);
+	else
+		return (0);
+}
