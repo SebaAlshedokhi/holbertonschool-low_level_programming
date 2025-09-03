733b45530942fb3818f06a22c76b3a9a48bb5210
root <root@ip-10-42-15-1.ec2.internal>
2025-09-03 15:30:51 +0000

    Update and Fix

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+/**
+* print_rev - prints a string.
+* @*s: pointer
+* Description: a function that prints a string, in reverse.
+*/
+
+#include"main.h"
+void print_rev(char *s)
+{
+	int i;
+	int length = 0;
+	str = str - 1;
+
+	for (i = 0 ; s[i] != '\0'; i++)
+		length++;
+	for (str[length - 1]; str[0]; str--)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
