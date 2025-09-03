b30a6c49ac0baeef390cd7ac26d42743c22530b6
root <root@ip-10-42-15-1.ec2.internal>
2025-09-03 16:15:37 +0000

    Update and Fix

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+/**
+* rev_string - prints a string.
+* @s: pointer
+* Description: a function that reverses a string.
+*/
+
+#include"main.h"
+void rev_string(char *s)
+{
+	int i;
+	int j;
+	int length = 0;
+	char c[long];
+
+	for (i=0 ; s[i] != '\0'; i++)
+	length++;
+
+	for (i = 0; i <= length; i++)
+	{
+	c[i] = s[i];
+	}
+
+	for (i = 0; i < length; i++)
+		for (j = (length - 1); j = 0; j--)
+			s[i] = c[j];
+}
