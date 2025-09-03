aa7d6288ad4fbb327914c8bfcc42e02fcf3b8cdf
root <root@ip-10-42-15-1.ec2.internal>
2025-09-03 14:55:36 +0000

    Update and Fix

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 /**
 * reset_to_98 - updates the value it points to to.
-* @*n: pointer
+* @n: pointer
 * Description: a function that takes a pointer to an int
 * as parameter and updates the value it points to to 98.
 */
