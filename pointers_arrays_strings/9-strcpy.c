7115959f2f7dae735d2e6433985a222568b15134
root <root@ip-10-42-189-68.ec2.internal>
2025-09-04 21:38:53 +0000

    Update and Fix

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+/**
+* _strcpy - copies the string to the buffe.
+* @dest: pointer
+* @src: pointer
+* Description: a function that copies the string
+* pointed to by src, including the terminating null byte (\0),
+* to the buffer pointed to by dest.
+* Return: the pointer to dest.
+*/
+
+#include"main.h"
+char *_strcpy(char *dest, char *src)
+{
+	int i, j;
+	int flength = 0;
+
+	for (i = 0 ; src[i] = '\0'; i++)
+	flength++;
+
+	for (i = 0; i <= flength; i++)
+	dest[i] = src[i];
+
+	return (*dest) ;
+}
