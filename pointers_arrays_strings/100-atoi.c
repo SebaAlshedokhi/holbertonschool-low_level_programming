08fe5126f4966e50a757e6caeec5a9f252b36af1
root <root@ip-10-42-11-132.ec2.internal>
2025-09-06 01:01:36 +0000

    Update and Fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,7 +11,8 @@ int _atoi(char *s)
 	int sign = 1;
 	int result = 0;
 	int num_started = 0;
-	
+	int digit;
+
 	while (*s)
 	{
 		if (*s == '-' && !num_started)
@@ -21,9 +22,9 @@ int _atoi(char *s)
 		else if (*s >= '0' && *s <= '9')
 		{
 			num_started = 1;
-			int digit = *s - '0';
+			digit = *s - '0';
 			if (sign == 1 && result > (2147483647 - digit) / 10)
-				return (214748364)7;
+				return (2147483647);
 			if (sign == -1 && result > (2147483647 - digit + 1) / 10)
 				return (-2147483648);
 			result = result * 10 + digit;
