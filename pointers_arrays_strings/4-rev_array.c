327ffd6ebec93145c8d6321d159703a2e59daa43
root <root@ip-10-42-242-168.ec2.internal>
2025-09-06 17:43:40 +0000

    Update and Fix

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+/**
+* reverse_array - reverses the content.
+* @a: pointer
+* @n: number of elements of the array
+* Description: a function that reverses the content of an array of integers.
+*/
+
+#include"main.h"
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int j = n - 1;
+	int rev;
+
+	while ( i < j )
+	{
+		rev = s[i];
+		a[i] = a[j];
+		a[j] = rev;
+		i++;
+		j--;
+	}
+}
