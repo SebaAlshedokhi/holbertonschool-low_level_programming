bbc10ee1207f238c67790e0dc2e3956dbefbf841
root <root@ip-10-42-11-132.ec2.internal>
2025-09-06 01:31:45 +0000

    Update and Fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -21,11 +21,11 @@ int _atoi(char *s)
 		else if (*s >= '0' && *s <= '9')
 		{
 			num_started = 1;
-			result = result * 10 + (*s - '0');
+			result = result * 10 + (*s - '0') * sign;
 		}
 		else if (num_started)
 			break;
 		s++;
 	}
-	return (sign * result);
+	return (result);
 }
