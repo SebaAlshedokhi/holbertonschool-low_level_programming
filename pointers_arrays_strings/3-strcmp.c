6effa576f8923cb44a4632225922476f43ec1fa2
root <root@ip-10-42-242-168.ec2.internal>
2025-09-06 17:27:14 +0000

    Update and Fix

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+/**
+* _strncmp - compares two strings.
+* @s1: pointer
+* @s2: pointer
+* Description: a function that compares two strings.
+* Return: an integer.
+*/
+
+#include"main.h"
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int result;
+
+	while (s1[i] == s2[i] && s[i] != '\0')
+	{
+	i++;
+	}
+	result = s1[i] - s2[i];
+	return (result);
+}
