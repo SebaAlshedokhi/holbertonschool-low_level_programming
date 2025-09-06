247dc781d7a4e9b774db2db8f703b7127a0c4117
root <root@ip-10-42-242-168.ec2.internal>
2025-09-06 14:29:55 +0000

    Update and Fix

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+/**
+* *_strcat - concatenates two strings.
+* @dest: pointer
+* @src: pointer
+* Description: a function that concatenates two strings.
+* Return: a pointer to the resulting string dest.
+*/
+
+#include<main.h>
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+	char *start = dest;
+	int length = 0;
+
+	for (i = 0; dest[i] != '\0', i++)
+		length++;
+ 
+	while (src != '\0')
+	{
+		*(dest + length) = src;
+		dest++;
+		src++;
+	}
+	*(dest + length) = '\0';
+	return (dest);
+}
