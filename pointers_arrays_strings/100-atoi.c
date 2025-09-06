c5318389c81beb636969687cee7b4a969658e343
root <root@ip-10-42-11-132.ec2.internal>
2025-09-06 01:04:59 +0000

    Update and Fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,30 +8,20 @@
 #include "main.h"
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
-	int num_started = 0;
-	int digit;
+	int sign=1,result=0;
 
 	while (*s)
 	{
-		if (*s == '-' && !num_started)
-			sign *= -1;
-		else if (*s == '+' && num_started)
+		if (*s=='-')
+			sign*=-1;
+		else if (*s>='0'&&*s<='9')
 			break;
-		else if (*s >= '0' && *s <= '9')
-		{
-			num_started = 1;
-			digit = *s - '0';
-			if (sign == 1 && result > (2147483647 - digit) / 10)
-				return (2147483647);
-			if (sign == -1 && result > (2147483647 - digit + 1) / 10)
-				return (-2147483648);
-			result = result * 10 + digit;
-		}
-		else if (num_started)
-			break;
+	s++;
+	}
+	while (*s>='0'&&*s<='9')
+	{
+		result=result*10+(*s-'0');
 		s++;
 	}
-	return sign * result;
+return sign*result;
 }
