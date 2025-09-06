217fafd71cb3767d78c5f38ee2e4f1c334449aa8
root <root@ip-10-42-242-168.ec2.internal>
2025-09-06 16:22:49 +0000

    Update and Fix

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+/**
+* *_strncpo - copies a strings.
+* @dest: pointer
+* @src: pointer
+* @n: integer
+* Description: a function that copies a string.
+* Return: a pointer to the resulting string dest.
+*/
+
+#include"main.h"
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *start = dest;
+	int count = 0;
+
+	while (*src != '\0' && count < n)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		count++;
+	}
+	while (count < n)
+	{
+		*dest = '\0';
+		dest++;
+		count++;
+	}
+	dest = start;
+	return (dest);
+}
