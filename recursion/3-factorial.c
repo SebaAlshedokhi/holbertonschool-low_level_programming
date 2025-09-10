ea5b258f8628dde805000092f06bd4cd86543ef4
root <root@ip-10-42-237-34.ec2.internal>
2025-09-10 13:33:37 +0000

    Update and Fix

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -8,12 +8,12 @@
 #include"main.h"
 int factorial(int n)
 {
-	int factorial = 1;
+	int fact = 1;
 
 	if (n < 0)
 		return (-1);
 	else if (n == 0)
 		return (1);
-	factorial = n * (factorial(n - 1));
-	return (factorial);
+	fact = n * (factorial(n - 1));
+	return (fact);
 }
