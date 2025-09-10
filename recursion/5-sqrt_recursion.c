513a9fcda2a0612a49e1a9341fb7885dabc15915
root <root@ip-10-42-237-34.ec2.internal>
2025-09-10 14:53:39 +0000

    Update and Fix

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -8,11 +8,16 @@
 #include"main.h"
 int _sqrt_recursion(int n)
 {
-	int x = 0;
-
-	if (n < 0 || x * x > n)
+	if (n < 0)
 		return (-1);
-	if (x * x == n)
+	return _sqrt_helper(n, 0);
+}
+
+int _sqrt_helper(int n, int x)
+{
+	if ((x * x) == n)
 		return (x);
-	_sqrt_recursion(x + 1);
+	if ((x * x) > n)
+		return (-1);
+	return (_sqrt_helper(n, x + 1));
 }
