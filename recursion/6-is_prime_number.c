462a66e75a83bb35a8877e59d9cf351912c22e4c
root <root@ip-10-42-237-34.ec2.internal>
2025-09-10 16:08:44 +0000

    Update and Fix

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,3 +1,6 @@
+int _prime_helper(int n, int divisor, int limit);
+int _sqrt_recursion(int n);
+
 /**
 * is_prime_number - tests if the number is prime.
 * @n: number
@@ -8,7 +11,25 @@
 #include"main.h"
 int is_prime_number(int n)
 {
+	if (n <= 1)
+		return (0);
+	return (prime_helper(int n, 2, _sqrt_recursion(n)));
+}
 
+/**
+* prime_helper - helper function.
+* @n: number
+* @divisor: number
+* @limit: number
+* Description: a function that help prime.
+* Return: result.
+*/
 
-return
+int prime_helper(int n, int divisor, int limit)
+{
+	if (divisor > limit)
+		return (1);
+	if (n % divisor == 0)
+		return (0);
+	return prime_helper(n, divisor + 1, limit);
 }
