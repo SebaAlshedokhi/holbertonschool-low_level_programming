fc9ede7ea2daf30e42ad9a8bae576849cdd2a8b8
root <root@ip-10-42-103-244.ec2.internal>
2025-09-13 17:41:09 +0000

    Update and Fix

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,12 +1,15 @@
 /**
- * main - prints the number of arguments
- * Description: a program that prints the number of arguments passed into it.
- * Return: Always 0
- */
+* main - prints the number of arguments
+* Description: a program that prints the number of arguments passed into it.
+* @argc: count
+* @argv: vector
+* Return: Always 0
+*/
 
 #include<stdio.h>
 int main(int argc, char *argv[])
 {
-	printf("%d\n",argc);
+	(void)argv;
+	printf("%d\n", argc);
 	return (0);
 }
