3208619ecc7c14ff5bd959d3906236a3e4a19d5c
root <root@ip-10-42-103-244.ec2.internal>
2025-09-13 19:11:45 +0000

    Update and Fix

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -15,10 +15,10 @@ int main(int argc, char *argv[])
 
 	if (argc == 1)
 	{
-		printf("%d\n",0);
+		printf("%d\n", 0);
 		return (0);
 	}
-	for (i = 1; i < argc ; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
