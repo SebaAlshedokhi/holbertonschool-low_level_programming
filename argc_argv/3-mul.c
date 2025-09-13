a4ba00a56056321e92096c4181f8e5bcc8604a2f
root <root@ip-10-42-103-244.ec2.internal>
2025-09-13 17:49:29 +0000

    Update and Fix

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,8 +1,10 @@
 /**
- * main - multiplies two numbers
- * Description: a program that multiplies two numbers.
- * Return: number
- */
+* main - multiplies two numbers
+* Description: a program that multiplies two numbers.
+* @argc: count
+* @argv: vector
+* Return: number
+*/
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -11,8 +13,11 @@ int main(int argc, char *argv[])
 	int mult;
 
 	if (argc < 3)
+	{
 		printf("Error\n");
+		return (0);
+	}
 	mult = atoi(argv[1]) * atoi(argv[2]);
-	printf("%d\n",mult);
+	printf("%d\n", mult);
 	return (0);
 }
