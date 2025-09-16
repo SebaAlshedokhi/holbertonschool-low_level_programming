cc234125e8b69a8a9e277d47d5b527d35cfd7841
root <root@ip-10-42-60-147.ec2.internal>
2025-09-16 14:31:37 +0000

    Update and Fix

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,14 +12,13 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
-	int i;
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
 	ptr = malloc(size * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
-	start = ptr;
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
