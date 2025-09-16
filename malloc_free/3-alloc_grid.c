52ba1526c057f4e8df92af646b4ccb03b6809407
root <root@ip-10-42-60-147.ec2.internal>
2025-09-16 16:29:25 +0000

    Update and Fix

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -14,25 +14,22 @@ int **alloc_grid(int width, int height)
 	int **tab, i, j;
 
 	if (width <= 0 || height <= 0)
-		return NULL;
+		return (NULL);
 	tab = malloc(sizeof(*tab) * height);
 	if (tab == NULL)
 		return (NULL);
-	else
+	for (i = 0; i < height; i++)
 	{
-		for (i = 0; i < height; i++)
+		tab[i] = malloc(sizeof(**tab) * width);
+		if (tab[i] == 0)
 		{
-			tab[i] = malloc(sizeof(**tab) * width);
-			if (tab[i] == 0)
-			{
-				while (i--)
-					free(tab[i]);
-				free(tab);
-				return (NULL);
-			}
-			for (j = 0; j < width; j++)
-				tab [i][j] = 0;
+			while (i--)
+				free(tab[i]);
+			free(tab);
+			return (NULL);
 		}
+		for (j = 0; j < width; j++)
+			tab[i][j] = 0;
 	}
 	return (tab);
 }
