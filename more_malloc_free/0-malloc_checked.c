a82794df996b046c09eec0909b60f18016396c23
root <root@ip-10-42-232-206.ec2.internal>
2025-09-17 15:53:57 +0000

    Update and Fix

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,17 +1,16 @@
 /**
- * *malloc_checked - allocates memory using malloc
- * @b: number
- * Description: a function that allocates memory using malloc.
- * Return: pointer
- */
+* *malloc_checked - allocates memory using malloc
+* @b: number
+* Description: a function that allocates memory using malloc.
+* Return: pointer
+*/
 
- #include<stdio.h>
- #include<stdlib.h>
- void *malloc_checked(unsigned int b)
- {
- 	int *ptr = malloc(b);
-
+#include<stdio.h>
+#include<stdlib.h>
+void *malloc_checked(unsigned int b)
+{	int *ptr = malloc(b);
+	
 	if (ptr == NULL)
 		exit (98);
 	return (ptr);
- }
+}
