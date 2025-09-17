7157c7e9ec439dbf073a6622f6b5de14c5dd44cf
root <root@ip-10-42-232-206.ec2.internal>
2025-09-17 16:27:04 +0000

    Update and Fix

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,10 +10,11 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
+	unsigned char *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	unsigned char *ptr= malloc(nmemb * size);
+	ptr= malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
