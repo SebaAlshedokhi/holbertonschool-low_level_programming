0010603357c1092206aa20966c545d775b61a40b
root <root@ip-10-42-113-141.ec2.internal>
2025-09-20 19:18:53 +0000

    Update and Fix

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -7,10 +7,12 @@
  * Description: a function that initialize a variable of type struct dog
  */
 #include"dog.h"
-#include<stdio.h>
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-d->name = name;
-d->age = age;
-d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
