823f9cb672947ea0f9c00e47e3327d0462c12737
root <root@ip-10-42-113-141.ec2.internal>
2025-09-20 17:03:34 +0000

    Update and Fix

---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - Dog data
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Owner name
+ *
+ * Description: a structure to recognize the dog data
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
