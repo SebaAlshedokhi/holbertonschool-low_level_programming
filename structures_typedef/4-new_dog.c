9795f3bb7d47b55957963fef5d1f48a9f8f3edfe
root <root@ip-10-42-113-141.ec2.internal>
2025-09-20 19:57:44 +0000

    Update and Fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,6 +9,7 @@
 int _strlen(const char *str)
 {
 	int length = 0;
+
 	while (*str++)
 		length++;
 	return (length);
@@ -24,6 +25,7 @@ int _strlen(const char *str)
 char *_strcopy(char *dest, char *src)
 {
 	int i;
+
 	for (i = 0; src[i]; i++)
 		dest[i] = src[i];
 	dest[i] = '\0';
@@ -44,23 +46,24 @@ char *_strcopy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+
 	if (!name || age < 0 || !owner)
 		return (NULL);
 	dog = (dog_t *) malloc(sizeof(dog_t));
 	if (dog == NULL)
-		return(NULL);
+		return (NULL);
 	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if ((*dog).name == NULL)
 	{
 		free(dog);
-		return(NULL);
+		return (NULL);
 	}
 	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if ((*dog).owner == NULL)
 	{
 		free(dog->name);
 		free(dog);
-		return(NULL);
+		return (NULL);
 	}
 	dog->name = _strcopy(dog->name, name);
 	dog->age = age;
