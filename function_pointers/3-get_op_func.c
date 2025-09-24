3cbce77387bfc84cc9998ec63e8ceb2c6984b872
root <root@ip-10-42-211-32.ec2.internal>
2025-09-24 16:51:31 +0000

    Update and Fix

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -15,5 +15,13 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
-
+	int i = 0;
+	
+	while (i < 6)
+	{
+		if (*s == *ops[i].op)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,22 +1,39 @@
 /**
- *  main - program
- *  Description: a program that performs simple operations.
- *  Return: Alwayes 0
+ * main - program
+ * @argc: count
+ * @argv: string
+ * Description: a program that performs simple operations.
+ * Return: Always 0
  */
 #include<stdio.h>
 #include"3-calc.h"
 #include<stdlib.h>
-int main (void)
+int main(int argc, char *argv[])
 {
-	int a, b;
-	char operator;
+	int a, b, result;
+	char *operator;
+	int (*func)(int, int);
 
-	if (argc != 5)
+	if (argc != 4)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(98);
 	}
 	a = atoi(argv[1]);
 	operator = argv[2];
 	b = atoi(argv[3]);
-	
+	func = get_op_func(operator);
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*operator == '/' || *operator == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = func(a, b);
+	printf("%d\n", result);
+}
+
