29e2bdbc535dd0f4b22752d6084fda7785fe28c6
root <root@ip-10-42-94-160.ec2.internal>
2025-09-25 20:39:30 +0000

    Update and Fix

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -21,7 +21,7 @@ int (*get_op_func(char *s))(int, int)
 	
 	while (i < 6)
 	{
-		if (*s[0] == *ops[i].op && s[1] == '\0')
+		if (s[0] == ops[i].op && s[1] == '\0')
 			return (ops[i].f);
 		i++;
 	}
