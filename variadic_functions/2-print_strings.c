d4e06d42e4cce25317fb32ac3f46d6442dbd3b73
root <root@ip-10-42-86-121.ec2.internal>
2025-09-27 17:19:04 +0000

    Update and Fix

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -5,22 +5,22 @@
 * print_strings - prints strings with seperator
 * @seperator: the seperator
 * @n: the number of arguments
-* @...: the strings to sum
 * Description: a function that prints strings with seperator
 */
 void print_strings(const char *seperator, const unsigned int n, ...)
 {
-	int i = n;
+	unsigned int i = n;
 	char *str;
 	va_list ap;
 
-	if (!n)
-	{
-		printf("\n");
-		return;
-	}
 	va_start(ap, n);
-	while (i--)
-		printf("%s%s", (str = va_arg(ap, char *)) ? str : "(nil)", i ? (seperator : "") : "\n");
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char *);
+		printf("%s", str ? str : "(nil)");
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
 	va_end(ap);
 }
