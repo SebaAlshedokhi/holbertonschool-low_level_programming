d00d6f9eaad5cc95408e5bdb0d71b626ab90ecf1
root <root@ip-10-42-86-121.ec2.internal>
2025-09-27 17:00:48 +0000

    Update and Fix

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -8,7 +8,7 @@
 * Description: a function that sums variable arguments
 * Return: the sum
 */
-int sum_them_all(const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	int s = 0, i = n;
 	va_list ap;
