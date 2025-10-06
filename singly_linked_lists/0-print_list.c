64cf396d17d565b1ee222b519aebb2374852c62c
root <root@ip-10-42-244-66.ec2.internal>
2025-10-06 17:08:47 +0000

    Update and Fix

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include"lists.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<stddef.h>
+#include <string.h>
+/**
+ * print_list - prints all the elements
+ * @h: list
+ * Description: a function that prints all the elements of a list_t list.
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t counter = 0;
+
+    while (h)
+    {
+	    if (h->str == NULL)
+	    printf("[%d] (%s)\n", 0, "nil");
+    else
+	    printf("[%d] (%s)\n", h->len, h->str);
+    h = h->next;
+    counter++;
+	}
+	return (counter);
+}
