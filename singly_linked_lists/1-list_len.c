89860c5e0bf3c52f548a15794d4fdd6acaffafc9
root <root@ip-10-42-244-66.ec2.internal>
2025-10-06 17:22:03 +0000

    Update and Fix

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include"lists.h"
+/**
+ * list_len - returns the number of elements
+ * @h: list
+ * Description: a function that returns the number of elements in a list_t list.
+ * Return: the number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t counter = 0;
+
+	while (h)
+	{
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
