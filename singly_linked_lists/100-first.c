7c82d4f87629addcff22625483ef72c3daf991a6
root <root@ip-10-42-24-161.ec2.internal>
2025-10-07 17:37:59 +0000

    Update and Fix

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include"lists.h"
+/**
+ * _construct - before main executed
+ * Description: a function prints befor main executed
+ */
+__attribute__((constructor))
+void _construc(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
