9618c89fc2d1b2f67e21a9676a0e699635f4a6fc
root <root@ip-10-42-24-161.ec2.internal>
2025-10-07 15:36:57 +0000

    Update and Fix

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,10 +10,17 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newnode;
-
+	int i;
+	unsigned int length = 0;
+	
 	newnode = malloc(sizeof(list_t));
 	newnode->str = strdup(str);
-	newnode->len = strlen(str);
+	while (str[i])
+	{
+		length++;
+		i++;
+	}
+	newnode->len = length;
 	newnode->next = *head;
 	*head = newnode;
 	return (newnode);
