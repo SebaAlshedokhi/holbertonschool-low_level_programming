16cfad7c28063a662a206a19b4608f0f23a47a55
root <root@ip-10-42-24-161.ec2.internal>
2025-10-07 15:53:35 +0000

    Update and Fix

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -6,22 +6,30 @@
 * Description: a function that adds a new node at the beginning
 * Return: the address of the new element, or NULL if it failed
 */
-
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *newnode;
-	int i;
-	unsigned int length = 0;
-	
-	newnode = malloc(sizeof(list_t));
-	newnode->str = strdup(str);
+	list_t *new_node;
+	int i = 0;
+	size_t length = 0;
+
+	if (str == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	while (str[i])
 	{
 		length++;
 		i++;
 	}
-	newnode->len = length;
-	newnode->next = *head;
-	*head = newnode;
-	return (newnode);
+	new_node->len = length;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
