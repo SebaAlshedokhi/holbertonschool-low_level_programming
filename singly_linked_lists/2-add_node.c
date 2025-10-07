25888f648fdaabb9d9fc152f9f821cc0426a8df6
root <root@ip-10-42-24-161.ec2.internal>
2025-10-07 15:32:39 +0000

    Update and Fix

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,20 @@
+#include"lists.h"
+/**
+* add_node - adds a new node at the beginning
+* @head: head
+* @str: char
+* Description: a function that adds a new node at the beginning
+* Return: the address of the new element, or NULL if it failed
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newnode;
+
+	newnode = malloc(sizeof(list_t));
+	newnode->str = strdup(str);
+	newnode->len = strlen(str);
+	newnode->next = *head;
+	*head = newnode;
+	return (newnode);
+}
