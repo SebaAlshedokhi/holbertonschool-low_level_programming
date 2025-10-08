44280ba5c970e39d1124d6d1ee944b963236b48e
root <root@ip-10-42-117-216.ec2.internal>
2025-10-08 16:36:51 +0000

    Update and Fix

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,22 @@
+#include"lists.h"
+/**
+* add_dnodeint - adds a new node at the beginning
+* @head: head
+* @n: int
+* Description: a function that adds a new node at the beginning
+* Return: the address of the new element, or NULL if it failed
+*/
+
+dlistint_t *add_dnodeint(list_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
