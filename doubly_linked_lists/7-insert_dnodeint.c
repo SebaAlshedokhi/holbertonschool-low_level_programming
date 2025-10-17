2aaf20ae72c6f4db4af2b18f749e968a2e4e2ef8
root <root@ip-10-42-77-165.ec2.internal>
2025-10-17 19:12:35 +0000

    Update and Fix

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,40 +1,45 @@
-#include"lists.h"
+#include "lists.h"
+#include <stdlib.h>
+
 /**
-* insert_dnodeint_at_index - inserts a new node at a given position
-* @h: head
-* @idx: index
-* @n: int
-* Description: a function that inserts a new node at a given position
-* Return: the address of the new node, or NULL if it failed
-*/
+	* insert_dnodeint_at_index - insert node at given index
+	* @h: head
+	* @idx: index
+	* @n: value
+	* Return: address of new node or NULL
+	*/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp, *new_node;
-	unsigned int i = 0;
+	dlistint_t *new_node, *tmp = *h;
+	unsigned int i;
 
+	if (!h)
+		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
-	if (!head || !new_node)
+	if (!new_node)
 		return (NULL);
 	new_node->n = n;
-	new_node->next = NULL;
-	if (!idx)
+	if (idx == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
+		new_node->prev = NULL;
+		new_node->next = *h;
+		if (*h)
+			(*h)->prev = new_node;
+		*h = new_node;
 		return (new_node);
 	}
-	tmp = *head;
-	while (tmp)
-	{
-		if (i == idx -1)
-		{
-			new_node->next = tmp->next;
-			tmp->next = new_node;
-			return (new_node);
-		}
-		i++;
+	for (i = 0; tmp && i < idx - 1; i++)
 		tmp = tmp->next;
+	if (!tmp)
+	{
+		free(new_node);
+		return (NULL);
 	}
-	free(new_node)
-	return (NULL);
+	new_node->next = tmp->next;
+	new_node->prev = tmp;
+	if (tmp->next)
+		tmp->next->prev = new_node;
+	tmp->next = new_node;
+	return (new_node);
 }
+
