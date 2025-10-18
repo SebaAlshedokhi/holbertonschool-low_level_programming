b721d607bbf1d776b5fe4090069d3d05b7ea0489
root <root@ip-10-42-90-192.ec2.internal>
2025-10-18 18:16:13 +0000

    full code 1st try

---
+++ file_io/3-cp.c
@@ -0,0 +1,61 @@
+#include "main.h"
+/**
+ * main - copies the content of a file to another file
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, exits with codes on failure
+ */
+int main(int argc, char *argv[])
+{
+	int from_fd, to_fd;
+	ssize_t bytes_read, bytes_written;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	from_fd = open(argv[1], O_RDONLY);
+	if (from_fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	to_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (to_fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(from_fd);
+		exit(99);
+	}
+	while ((bytes_read = read(from_fd, buffer, 1024)) > 0)
+	{
+		bytes_written = write(to_fd, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(from_fd);
+			close(to_fd);
+			exit(99);
+		}
+	}
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(from_fd);
+		close(to_fd);
+		exit(98);
+	}
+	if (close(from_fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_fd);
+		exit(100);
+	}
+	if (close(to_fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_fd);
+		exit(100);
+	}
+	return (0);
+}
