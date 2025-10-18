0aacfc61ddd677a753daec04c7fee92c275a1128
root <root@ip-10-42-90-192.ec2.internal>
2025-10-18 18:31:03 +0000

    fix function length add small func

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,6 +1,102 @@
 #include "main.h"
 /**
- * main - copies the content of a file to another file
+ * check_arguments - ensures correct number of arguments
+ * @argc: argument count
+ */
+void check_arguments(int argc)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+}
+
+/**
+ * open_source_file - opens file for reading
+ * @filename: name of source file
+ * Return: file descriptor
+ */
+int open_source_file(const char *filename)
+{
+	int fd;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	return (fd);
+}
+
+/**
+ * open_dest_file - opens/creates file for writing
+ * @filename: name of destination file
+ * Return: file descriptor
+ */
+int open_dest_file(const char *filename)
+{
+	int fd;
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
+	return (fd);
+}
+
+/**
+ * copy_content - copies content from one fd to another
+ * @from_fd: source file descriptor
+ * @to_fd: destination file descriptor
+ * @file_from: source filename (for error messages)
+ * @file_to: destination filename (for error messages)
+ */
+void copy_content(int from_fd, int to_fd, const char *file_from, const char *file_to)
+{
+	char buffer[1024];
+	ssize_t bytes_read, bytes_written;
+
+	bytes_read = read(from_fd, buffer, 1024);
+	while (bytes_read > 0)
+	{
+		bytes_written = write(to_fd, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			close(from_fd);
+			close(to_fd);
+			exit(99);
+		}
+		bytes_read = read(from_fd, buffer, 1024);
+	}
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		close(from_fd);
+		close(to_fd);
+		exit(98);
+	}
+}
+
+/**
+ * close_fd - closes a file descriptor with error checking
+ * @fd: file descriptor
+ */
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - copies content of a file to another file
  * @argc: argument count
  * @argv: argument vector
  * Return: 0 on success, exits with codes on failure
@@ -8,54 +104,11 @@
 int main(int argc, char *argv[])
 {
 	int from_fd, to_fd;
-	ssize_t bytes_read, bytes_written;
-	char buffer[1024];
-
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	from_fd = open(argv[1], O_RDONLY);
-	if (from_fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	to_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (to_fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(from_fd);
-		exit(99);
-	}
-	while ((bytes_read = read(from_fd, buffer, 1024)) > 0)
-	{
-		bytes_written = write(to_fd, buffer, bytes_read);
-		if (bytes_written == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(from_fd);
-			close(to_fd);
-			exit(99);
-		}
-	}
-	if (bytes_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(from_fd);
-		close(to_fd);
-		exit(98);
-	}
-	if (close(from_fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_fd);
-		exit(100);
-	}
-	if (close(to_fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_fd);
-		exit(100);
-	}
+	check_arguments(argc);
+	from_fd = open_source_file(argv[1]);
+	to_fd = open_dest_file(argv[2]);
+	copy_content(from_fd, to_fd, argv[1], argv[2]);
+	close_fd(from_fd);
+	close_fd(to_fd);
 	return (0);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -10,4 +10,9 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void check_arguments(int argc);
+int open_source_file(const char *filename);
+int open_dest_file(const char *filename);
+void copy_content(int from_fd, int to_fd, const char *file_from, const char *file_to);
+void close_fd(int fd);
 #endif
