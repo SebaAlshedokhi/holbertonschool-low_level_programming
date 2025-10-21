0af7e83c1c7495246d7acb2ab7a80d4880e814d3
root <root@ip-10-42-112-246.ec2.internal>
2025-10-21 16:57:53 +0000

    Update and Fix

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -18,13 +18,13 @@ typedef struct hash_node_s
 } hash_node_t;
 
 /**
- * struct hash_table_s - Hash table data structure
- *
- * @size: The size of the array
- * @array: An array of size @size
- * Each cell of this array is a pointer to the first node of a linked list,
- * because we want our HashTable to use a Chaining collision handling
- */
+* struct hash_table_s - Hash table data structure
+*
+* @size: The size of the array
+* @array: An array of size
+* Each cell of this array is a pointer to the first node of a linked list,
+* because we want our HashTable to use a Chaining collision handling
+*/
 typedef struct hash_table_s
 {
 	unsigned long int size;
