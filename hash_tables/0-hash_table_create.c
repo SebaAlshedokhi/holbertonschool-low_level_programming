b127ad1f093ed0d3a684b3317b436813e8b37688
root <root@ip-10-42-112-246.ec2.internal>
2025-10-21 16:46:31 +0000

    Update and Fix

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+/**
+* *hash_table_create -creates a hash table
+* @size: size of the array
+* Description: a function that creates a hash table.
+* Return: a pointer to the newly created hash table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t hash_table;
+	unsigned long int i = 0;
+
+	if (size == 0)
+		return (NULL);
+	hash_table = malloc(sizeof(hash_table_t));
+	if (!hash_table)
+		return (NULL);
+	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!hash_table->array)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+	while (i < size)
+	{
+		hash_table->array[i] = NULL;
+		i++;
+	}
+	return (hash_table);
+}
