1d8f8b9ae44dcffcb8c736c70ec1ba3cb6011fca
root <root@ip-10-42-178-34.ec2.internal>
2025-10-22 11:36:13 +0000

    Update and Fix

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -9,8 +9,40 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_table_t *tmp;
-	
-	if (key == NULL)
+	unsigned long int index;
+	hash_node_t *node;
+	char *vcopy;
+
+	if (!ht || !key || !*key || !value)
 		return (0);
-	tmp = malloc(sizeof(hash_table_t));
+	vcopy = strdup(value);
+	if (!vcopy)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	for (node = ht->array[index]; node; node = node->next)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value);
+			node->value = vcopy;
+			return (1);
+		}
+	}
+	node = malloc(sizeof(*node));
+	if (!node)
+	{
+		free(vcopy);
+		return (0);
+	}
+	node->key = strdup(key);
+	if (!node->key)
+	{
+		free(vcopy);
+		free(node);
+		return (0);
+	}
+	node->value = vcopy;
+	node->next = ht->array[index];
+	ht->array[index] = node;
+	return (1);
+}
